c1c9464ca9bc648a288547a1375a388bf876cff7
TheOdiumWitch <41388218+TheOdiumWitch@users.noreply.github.com>
2020-08-18 16:59:11 -0500

    Fixed crash (oopsie)

--- src/main/java/com/odium/marine/registry/ModItems.java
+++ src/main/java/com/odium/marine/registry/ModItems.java
@@ -1,6 +1,8 @@
 package com.odium.marine.registry;
 
 import com.odium.marine.Marine;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -10,7 +12,7 @@ import net.minecraft.util.registry.Registry;
 public class ModItems {
     public static final Item CEVICHE = new Item(new Item.Settings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(5).saturationModifier(6).build()));
     public static final Item FUGU = new Item(new Item.Settings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(8).saturationModifier(10).build()));
-
+    
 
     public static void RegisterItems(){
         Registry.register(Registry.ITEM, new Identifier(Marine.MOD_ID, "ceviche"), CEVICHE);
