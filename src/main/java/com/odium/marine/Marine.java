92711969882d07b98828b453f23d2be3c1021b95
TheOdiumWitch <41388218+TheOdiumWitch@users.noreply.github.com>
2020-08-18 16:09:09 -0500

    fugu and ceviche

---
+++ src/main/java/com/odium/marine/Marine.java
@@ -0,0 +1,14 @@
+package com.odium.marine;
+
+import com.odium.marine.registry.ModItems;
+import net.fabricmc.api.ModInitializer;
+
+public class Marine implements ModInitializer {
+
+    public static String MOD_ID = "mstuff";
+
+    @Override
+    public void onInitialize() {
+        ModItems.RegisterItems();
+    }
+}
---
+++ src/main/java/com/odium/marine/registry/ModItems.java
@@ -0,0 +1,22 @@
+package com.odium.marine.registry;
+
+import com.odium.marine.Marine;
+import net.minecraft.item.FoodComponent;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class ModItems {
+    public static final Item CEVICHE = new Item(new Item.Settings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(5).saturationModifier(6).build()));
+    public static final Item FUGU = new Item(new Item.Settings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(8).saturationModifier(10).build()));
+
+
+    public static void RegisterItems(){
+        Registry.register(Registry.ITEM, new Identifier(Marine.MOD_ID, "ceviche"), CEVICHE);
+        Registry.register(Registry.ITEM, new Identifier(Marine.MOD_ID, "fugu"), FUGU);
+
+    }
+}
+
+
