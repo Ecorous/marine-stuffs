3807bab5ac7b8eb14475cad511b0c41b66756c40
Expora9 <49562894+Expora9@users.noreply.github.com>
2020-08-18 20:14:42 +0100

    Update 1

---
+++ src/main/java/net/fabricmc/example/DeepSeaMaterial.java
@@ -0,0 +1,32 @@
+package net.fabricmc.example;
+
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.util.Lazy;
+
+import java.util.function.Supplier;
+
+public enum DeepSeaMaterial implements ArmorMaterial
+{
+    private static final int[] baseDurability = {13, 15, 16, 11};
+    private final String name;
+    private final int durabilityMultiplier;
+    private final int[] armorValues;
+    private final int enchantability;
+    private final SoundEvent equipSound;
+    private final float toughness;
+    private final float knockbackResistance;
+    private final Lazy<Ingredient> repairIngredient;
+
+    CustomArmorMaterial(String name, int durabilityMultiplier, int[] armorValueArr, int enchantability, SoundEvent soundEvent, float toughness, float knockbackResistance, Supplier<Ingredient> repairIngredient) {
+        this.name = name;
+        this.durabilityMultiplier = durabilityMultiplier;
+        this.armorValues = armorValueArr;
+        this.enchantability = enchantability;
+        this.equipSound = soundEvent;
+        this.toughness = toughness;
+        this.knockbackResistance = knockbackResistance;
+        this.repairIngredient = new Lazy(repairIngredient);
+    }
+}
