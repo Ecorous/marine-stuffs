72f010d4db47b34d18c1e7f4457aa028b9ca921f
Expora9 <49562894+Expora9@users.noreply.github.com>
2020-08-19 14:05:55 +0100

    ArmorItem

---
+++ src/main/java/com/odium/marine/armors/Armor.java
@@ -0,0 +1,12 @@
+package com.odium.marine.armors;                                                  
+                                                                                  
+import net.minecraft.entity.EquipmentSlot;                                        
+import net.minecraft.item.ArmorItem;                                              
+import net.minecraft.item.ArmorMaterial;                                          
+import com.odium.marine.Marine.ID;                                                
+                                                                                  
+public class Armor extends ArmorItem {
+    public Armor(ArmorMaterial material, EquipmentSlot slot, Settings settings) {
+        super(material, slot, settings); }
+    }
+
