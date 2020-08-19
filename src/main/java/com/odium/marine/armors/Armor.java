0312a8fc4e0a5e701fa7ecf82334e265f0977300
Expora9 <49562894+Expora9@users.noreply.github.com>
2020-08-19 14:12:26 +0100

    Removed a wrong line of code

--- src/main/java/com/odium/marine/armors/Armor.java
+++ src/main/java/com/odium/marine/armors/Armor.java
@@ -3,7 +3,7 @@ package com.odium.marine.armors;
 import net.minecraft.entity.EquipmentSlot;                                        
 import net.minecraft.item.ArmorItem;                                              
 import net.minecraft.item.ArmorMaterial;                                          
-import com.odium.marine.Marine.ID;                                                
+
                                                                                   
 public class Armor extends ArmorItem {
     public Armor(ArmorMaterial material, EquipmentSlot slot, Settings settings) {
