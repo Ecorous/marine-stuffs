698f514dbe17cc64ae3ff2acf66c47fb622462da
TheOdiumWitch <41388218+TheOdiumWitch@users.noreply.github.com>
2020-08-19 10:04:44 -0500

    DolphinMaterial

--- src/main/java/com/odium/marine/registry/ModItems.java
+++ src/main/java/com/odium/marine/registry/ModItems.java
@@ -2,6 +2,7 @@ package com.odium.marine.registry;
 
 import com.odium.marine.Marine;
 import com.odium.marine.armors.DeepSeaMaterial;
+import com.odium.marine.armors.DolphinMaterial;
 import com.odium.marine.armors.NautilusShellMaterial;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.*;
@@ -11,10 +12,19 @@ import net.minecraft.util.registry.Registry;
 public class ModItems {
     public static final Item CEVICHE = new Item(new Item.Settings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(5).saturationModifier(6).build()));
     public static final Item FUGU = new Item(new Item.Settings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(8).saturationModifier(10).build()));
+    public static final Item CALAMARI = new Item(new Item.Settings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(6).saturationModifier(6).build()));
+    public static final Item RAW_CALAMARI = new Item(new Item.Settings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(3).saturationModifier(1).build()));
+    public static final Item FISH_BOWL = new Item(new Item.Settings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(12).saturationModifier(14).build()));
+
+    public static final Item DOLPHIN_LEATHER = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
+
+    public static final Item DOLPHIN_BOOTS= new ArmorItem(DolphinMaterial.DolphinLeatherMaterial.DOLPHIN_LEATHER, EquipmentSlot.FEET, (new Item.Settings().group(ItemGroup.COMBAT)));
+
     public static final Item SEA_HELMET = new ArmorItem(DeepSeaMaterial.SeaArmorMaterial.SEA, EquipmentSlot.HEAD, (new Item.Settings().group(ItemGroup.COMBAT)));
     public static final Item SEA_CHESTPLATE = new ArmorItem(DeepSeaMaterial.SeaArmorMaterial.SEA, EquipmentSlot.CHEST, (new Item.Settings().group(ItemGroup.COMBAT)));
     public static final Item SEA_LEGGINGS = new ArmorItem(DeepSeaMaterial.SeaArmorMaterial.SEA, EquipmentSlot.LEGS, (new Item.Settings().group(ItemGroup.COMBAT)));
     public static final Item SEA_BOOTS = new ArmorItem(DeepSeaMaterial.SeaArmorMaterial.SEA, EquipmentSlot.FEET, (new Item.Settings().group(ItemGroup.COMBAT)));
+
     public static final Item SHELL_HELMET = new ArmorItem(NautilusShellMaterial.ShellArmorMaterial.SHELL, EquipmentSlot.HEAD, (new Item.Settings().group(ItemGroup.COMBAT)));
     public static final Item SHELL_CHESTPLATE = new ArmorItem(NautilusShellMaterial.ShellArmorMaterial.SHELL, EquipmentSlot.CHEST, (new Item.Settings().group(ItemGroup.COMBAT)));
     public static final Item SHELL_LEGGINGS = new ArmorItem(NautilusShellMaterial.ShellArmorMaterial.SHELL, EquipmentSlot.LEGS, (new Item.Settings().group(ItemGroup.COMBAT)));
@@ -26,10 +36,18 @@ public class ModItems {
 
         Registry.register(Registry.ITEM, new Identifier(Marine.ID, "ceviche"), CEVICHE);
         Registry.register(Registry.ITEM, new Identifier(Marine.ID, "fugu"), FUGU);
+        Registry.register(Registry.ITEM, new Identifier(Marine.ID, "calamari"), CALAMARI);
+        Registry.register(Registry.ITEM, new Identifier(Marine.ID, "raw_calamari"), RAW_CALAMARI);
+        Registry.register(Registry.ITEM, new Identifier(Marine.ID, "fish_bowl"), FISH_BOWL);
+        Registry.register(Registry.ITEM, new Identifier(Marine.ID, "dolphin_leather"), DOLPHIN_LEATHER);
+
+        Registry.register(Registry.ITEM, new Identifier(Marine.ID, "dolphin_boots"), DOLPHIN_BOOTS);
+
         Registry.register(Registry.ITEM, new Identifier(Marine.ID, "sea_helmet"), SEA_HELMET);
         Registry.register(Registry.ITEM,new Identifier(Marine.ID,"sea_chestplate"), SEA_CHESTPLATE);
         Registry.register(Registry.ITEM,new Identifier(Marine.ID,"sea_leggings"), SEA_LEGGINGS);
         Registry.register(Registry.ITEM,new Identifier(Marine.ID,"sea_boots"), SEA_BOOTS);
+
         Registry.register(Registry.ITEM, new Identifier(Marine.ID, "shell_helmet"), SHELL_HELMET);
         Registry.register(Registry.ITEM,new Identifier(Marine.ID,"shell_chestplate"), SHELL_CHESTPLATE);
         Registry.register(Registry.ITEM,new Identifier(Marine.ID,"shell_leggings"), SHELL_LEGGINGS);
